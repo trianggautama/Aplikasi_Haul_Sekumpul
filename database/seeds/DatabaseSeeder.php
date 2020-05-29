30682f0d49a39f5e4aa057534c60ce0c54975201
muhammadasadbastomi <tomycules@gmail.com>
2020-05-29 20:58:33 +0800

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,10 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UserSeeder::class);
         DB::table('users')->insert([
+            'uuid' => Str::random(36),
             'nama' => 'admin',
             'username' => 'admin',
+            'no_hp' => '087771787178',
             'role' => 2,
             'foto' => 'default.jpg',
             'password' => Hash::make('admin'),
